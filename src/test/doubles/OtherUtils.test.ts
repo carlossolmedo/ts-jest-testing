af19beb32623fdc070be16d906227189829ab8d8
Carlos Olmedo <carlos.olmedodev@gmail.com>
2024-04-16 17:48:58 +0200

    feat: test doubles -> mocks

--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -35,4 +35,55 @@ describe('OtherUtils test suite', () => {
       expect(actual).toBe('ABC');
     });
   });
+
+  describe('Tracking callbacks', () => {
+    let cbArgs: string[] = [];
+    let timesCalled = 0;
+
+
+    function callBackMock(arg: string) {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach(() => {
+      // clearing tracking fields
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it('tracks calls - should call the callback for invalid argument return undefined', () => {
+      const actual = toUpperCaseWithCb('', callBackMock); // mock callback
+      expect(actual).toBeUndefined();
+      expect(cbArgs).toContain('Invalid argument');
+      expect(timesCalled).toBe(1);
+    });
+    it('tracks calls - should call the callback for valid argument return the upper cased string', () => {
+      const actual = toUpperCaseWithCb('abc', callBackMock);
+      expect(actual).toBe('ABC');
+      expect(cbArgs).toContain('called toUpperCaseWithCb with abc');
+      expect(timesCalled).toBe(1);
+    });
+  });
+
+  describe.only('Tracking callbacks - with Jest', () => {
+    const callBackMock = jest.fn();
+
+    afterEach(() => {
+      // clearing mocks
+      jest.clearAllMocks();
+    });
+    it('tracks calls - should call the callback for invalid argument return undefined', () => {
+      const actual = toUpperCaseWithCb('', callBackMock);
+      expect(actual).toBeUndefined();
+      expect(callBackMock).toHaveBeenCalledWith('Invalid argument');
+      expect(callBackMock).toHaveBeenCalledTimes(1);
+    });
+    it('tracks calls - should call the callback for valid argument return the upper cased string', () => {
+      const actual = toUpperCaseWithCb('abc', callBackMock);
+      expect(actual).toBe('ABC');
+      expect(callBackMock).toHaveBeenCalledWith('called toUpperCaseWithCb with abc');
+      expect(callBackMock).toHaveBeenCalledTimes(1);
+    });
+  });
 });
